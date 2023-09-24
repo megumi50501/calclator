edd1d586e072f7e87a68d4447bb92409828e18fe
sinya13719 <sinya13719@gmail.com>
2023-09-24 12:09:25 +0900

    feat: 電卓アプリの全機能実装

--- app/src/main/java/jp/ac/hal/pi12a215_49_kadai_calculator/MainActivity.java
+++ app/src/main/java/jp/ac/hal/pi12a215_49_kadai_calculator/MainActivity.java
@@ -9,13 +9,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
-    //演算子フラグ
-    boolean opFlg = false;
-    //ひとつめの数値を記憶（退避用）
-    int wkNum;
-    //クリックした演算子を記憶
-    String opCode;
+    private TextView tv;
+
+    // 演算子フラグ
+    private boolean opFlg = false;
+
+    // 演算子コード
+    private String opCode = "";
+
+    // 画面初期化フラグ
+    private boolean resetRequired = false;
+
+    // ひとつめの数値を記憶（退避用）
+    private int firstNum;
+
+    // (+/-)反転回数
+    private int reverseCount = 0;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -24,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         // 表示領域の変数を宣言
         // 表示領域のインスタンス化
-        tv = (TextView) findViewById(R.id.outputArea);
+        tv = findViewById(R.id.outputArea);
 
         Button[] numBtn = new Button[10];
         //ボタン部品(０～９)のインスタンス化
@@ -41,65 +50,76 @@ public class MainActivity extends AppCompatActivity {
 
         //数値ボタン×１０にボタンクリックリスナを実装
         for (int i = 0; i < 10; i++) {
-            numBtn[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //数値ボタンがクリックされた時の処理
-                    //表示領域に数値が表示される
-                    //自作表示メソッドを利用
-                    displayNumber(v);
-                }
-            });
+            //数値ボタンがクリックされた時の処理
+            numBtn[i].setOnClickListener(this::displayNumber);
         }
 
         // クリアボタンのインスタンス化
-        Button clearBtn = (Button) findViewById(R.id.clear);
+        Button clearBtn = findViewById(R.id.clear);
         // クリアボタンの実装
-        clearBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // ディスプレイに`0`を表示する
-                tv.setText("0");
-            }
+        clearBtn.setOnClickListener((View v) -> {
+            // 演算子フラグを初期化する
+            opFlg = false;
+            // 演算子コードを初期化する
+            opCode = "";
+            // 画面初期化フラグを初期化する
+            resetRequired = false;
+            // 反転回数を初期化する
+            reverseCount = 0;
+            // ディスプレイに`0`を表示する
+            tv.setText(String.valueOf(0));
         });
 
-        Button[] opSymbol = new Button[4];
-        opSymbol[0] = findViewById(R.id.plus);
-        opSymbol[1] = findViewById(R.id.minus);
-        opSymbol[2] = findViewById(R.id.divided);
-        opSymbol[3] = findViewById(R.id.multiplied);
+        Button[] opSymbols = new Button[4];
+        opSymbols[0] = findViewById(R.id.plus);
+        opSymbols[1] = findViewById(R.id.minus);
+        opSymbols[2] = findViewById(R.id.divided);
+        opSymbols[3] = findViewById(R.id.multiplied);
 
         // 配列におさまった演算子にクリック時のリスナーを実装
-        for(int i=0; i<opSymbol.length ; i++ ) {
-            opSymbol[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //自作関数へ演算子クリック時の動作をまとめる
-                    opClicked(v);
-                    // もともとあったフラグをＯＮにする処理は、自作関数へお引越し
-                    // するため、コメントアウトしました
-                    // プラスボタンがクリックされたらすべきことは？
-                    // 次に数値が入力されたら、画面に表示する準備
-                    // 数値ボタンを押すアクションはすでに作成してあるはず・・・
-                    // 演算ボタンがクリックされた・されていないの状態フラグを利用
-                    opFlg = true;
-                }
-            });
+        for (Button opSymbol : opSymbols) {
+            // クリックされた演算子の処理
+            opSymbol.setOnClickListener(this::opClicked);
         }
 
-        //演算ボタンのインスタンス化
-        Button plusOp = findViewById(R.id.plus);
-        //ボタンクリック時のイベントを受け取るリスナー
-        plusOp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //プラスボタンがクリックされたらすべきことは？
-                //次に数値が入力されたら、画面に表示する準備
-                //数値ボタンを押すアクションはすでに作成してあるはず・・・
-                //数値ボタンを押すアクションはすでに作成してあるはず・・・
+        // イコールボタンのインスタンス化
+        Button equalOp = findViewById(R.id.equal);
+        // イコールボタンクリック時のイベントを受け取るリスナー
+        equalOp.setOnClickListener((View v) -> {
+            // 計算結果を画面に表示する
+            tv.setText(String.valueOf(calculate()));
+            resetRequired = true;
+            // 反転カウントを初期化する
+            reverseCount = 0;
+        });
+
+        // ±ボタンのインスタンス化
+        Button reverseOp = findViewById(R.id.plusminus);
+        reverseOp.setOnClickListener((View v) -> {
+            // 反転回数をカウントする
+            reverseCount++;
+            // 表示されている数値を取得
+            String tvStr = tv.getText().toString();
+            int displayedNum = Integer.parseInt(tvStr);
+            if (reverseCount % 2 == 1) {
+                tv.setText(String.valueOf(-displayedNum));
+            } else {
+                tv.setText(String.valueOf(displayedNum));
             }
         });
 
+        // バックスペースボタンのインスタンス化
+        Button backSpaceOp = findViewById(R.id.backspace);
+        backSpaceOp.setOnClickListener((View v) -> {
+            // 表示されている数値を取得
+            String tvStr = tv.getText().toString();
+            String editedNum = tvStr.substring(0, tvStr.length() - 1);
+            if(editedNum.length() == 0){
+                tv.setText(String.valueOf(0));
+            } else {
+                tv.setText(editedNum);
+            }
+        });
     }
 
     private void displayNumber(View v){
@@ -107,57 +127,67 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button) v;
         // ボタンに表示されているラベル（数値）を取得
         String str = btn.getText().toString();
-        // 取得した数字を数値に変換
-        int dNum = Integer.parseInt(str);
+        int pushedNum = Integer.parseInt(str);
         // 表示されている数値を取得
         String tvStr = tv.getText().toString();
-        int tvNum = Integer.parseInt(tvStr);
-        // 計算処理
-        int num = tvNum * 10 + dNum;
-        // ボタンの数値を画面に表示する
-        tv.setText(String.valueOf(num));
+        int displayedNum = Integer.parseInt(tvStr);
+
+        // 演算子フラグが`true`または演算子フラグが`false`でかつ演算子コードが指定されている場合は、
+        // 一つ目の数値を記憶し、二つ目の数値を画面に表示する
+        // それ以外の場合は、選択された数値を画面に表示する
+        if(opFlg && resetRequired){
+            firstNum = displayedNum;
+            tv.setText(String.valueOf(pushedNum));
+            resetRequired = false;
+        } else {
+            // 選択された数値を画面に表示する
+            tv.setText(String.valueOf(displayedNum * 10 + pushedNum));
+        }
     }
 
     //演算ボタンがクリックされた処理
     private void opClicked(View v){
-        //演算子ボタンがクリックされたらすべきことは？
-        //次に数値が入力されたら、画面に表示する準備
-        //数値ボタンを押すアクションはすでに作成してあるはず・・・
-        //演算ボタンがクリックされた・されていないの状態フラグを利用
+        //演算ボタンがクリックされた・されていないの状態フラグ
         opFlg = true;
+        resetRequired = true;
+        reverseCount = 0;
         //どの演算子ボタンがクリックされたかを記憶
         Button btn = (Button) v;
+
         opCode = btn.getText().toString();
     }
 
-    private void calculate(){
-        //➊ 一時保存で退避した１番目の数値と
-        //❷ 表示されている2番目の数値の計算を実行
-        String strNum = tv.getText().toString();
-        int secondNum = Integer.parseInt(strNum);
+    private int calculate(){
+        // 一時保存で退避した１番目の数値と表示されている2番目の数値の計算を実行
+        String str = tv.getText().toString();
+        int secondNum = Integer.parseInt(str);
         int answer = 0;
-        //演算記号による演算処理の分岐
-        //「＋」がクリックされている場合
-        if(opCode.equals("+")){
-            //➊ ＋ ❷
-            answer = wkNum + secondNum;
+
+        switch(opCode) {
+            //演算記号による演算処理の分岐
+            //「＋」がクリックされている場合
+            case "+":
+                //➊ ＋ ❷
+                answer = firstNum + secondNum;
+                break;
+            //「－」がクリックされている場合
+            case "-":
+                //➊ － ❷
+                answer = firstNum - secondNum;
+                break;
+
+            //「×」がクリックされている場合
+            case "×":
+                //➊ × ❷
+                answer = firstNum * secondNum;
+                break;
+
+            //「÷」がクリックされている場合
+            case "÷":
+                //➊ × ❷
+                answer = firstNum / secondNum;
         }
-        //「－」がクリックされている場合
-        else if (opCode.equals("－")) {
-            //➊ － ❷
-            answer = wkNum - secondNum;
-        }
-        //「×」がクリックされている場合
-        else if (opCode.equals("×")) {
-            //➊ × ❷
-            answer = wkNum * secondNum;
-        }
-        //「÷」がクリックされている場合
-        else if (opCode.equals("÷")) {
-            //➊ × ❷
-            answer = wkNum / secondNum;
-        }
-        //結果を表示する
-        tv.setText(String.valueOf(answer));
+
+        return answer;
     }
 }
